a85092bfb6ac6f3da35e9fb8ca210cc4c4aeaf40
shadow <matematicadivina1@gmail.com>
2022-03-02 21:38:44 -0500

    ya funciona el juego

--- src/componentes/Casilla/index.js
+++ src/componentes/Casilla/index.js
@@ -1,20 +1,25 @@
 import { useState } from "react"
 
 const Casilla = (props)=>{
-    const {player, callback } = props
-    const [content, setContent] = useState(1)
-    const _handlerClick = ()=>{
-        setContent(player)
-        callback()
-        console.log(player)
+    const {callback, player,id, gameState } = props
+    const [jugador, setJugador]=useState(null)
+
+    const _handlerClick = (e)=>{
+        if (!jugador && gameState==="en curso"){
+        setJugador(player===1? "x": "o")
+        callback(e)
+        }
     }
     return(
-        <div
-        style={{cursor:"pointer", border:"1px solid"}}
+        <div id={id}
+        style={{cursor:"pointer", border:"1px solid",height:80}}
         className="casilla" onClick={_handlerClick}>
-            <p>{content}</p>
+            <p>{jugador}</p>
         </div>
     )
 }
+
+//Usar usme y with meme par ano re renderizar las casillas
+
 
 export default Casilla
--- src/componentes/Tablero/index.js
+++ src/componentes/Tablero/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import  Casilla  from "../Casilla"
 import './styles.css'
 
@@ -8,41 +8,89 @@ const usePlayer = (initialState)=>{
     const tooglePlayer = ()=>{
         switch (figure) {
             case 1:
+                
                 setFigure(2)
                 break;
             case 2:
+                
                 setFigure(1)
                 break;
             default:
-                setFigure(1)
+                
+                setFigure(2)
                 break;
         }
-        console.log(figure+": figure")
+       
     }
     return [figure, tooglePlayer]
 }
 
+const casos = array=>
+(array[0][0]===array[1][1] && array[0][0]===array[2][2] && array[0][0]!==null) ||
+(array[0][2]===array[1][1] && array[0][2]===array[2][0] && array[0][2]!==null) ||
+
+(array[0][0]===array[1][0] && array[0][0]===array[2][0] && array[0][0]!==null) ||
+(array[0][1]===array[1][1] && array[0][1]===array[2][1] && array[0][1]!==null) ||
+(array[0][2]===array[1][2] && array[0][2]===array[2][2] && array[0][2]!==null) ||
+
+
+
+(array[0][0]===array[0][1] && array[0][0]===array[0][2] && array[0][0]!==null) ||
+(array[1][0]===array[1][1] && array[1][0]===array[1][2] && array[1][0]!==null) ||
+(array[2][0]===array[2][1] && array[2][0]===array[2][2] && array[2][0]!==null) 
+
 
 
 const Tablero = ()=>{
+    const [player, tooglePlayer] = usePlayer(1)
+    const [stateCasillas, setStateCasillas]=useState([[null, null, null],
+                                                       [null, null, null],
+                                                    [null, null, null] ])
+    
+    const [gameState, setGameState] =useState("en curso")
+    const callback=(e)=>{
+        
+        
+        if (gameState==="en curso"){
+            console.log(e)
+            
+            const casillaNumero = e.target.id.split("-")
+            const prevStateCasillas = stateCasillas
+            prevStateCasillas[casillaNumero[0]][casillaNumero[1]] = player
+            if (casos(prevStateCasillas)){setGameState("El ganador es el jugador "+ player)}
+            let flag= true
+            prevStateCasillas.forEach(e=>e.forEach(e=>flag =flag && e))
+            if(flag) {setGameState("Empate")} 
+            console.log("la bandera es" + flag)
+            setStateCasillas([...prevStateCasillas])
+            tooglePlayer() 
+        }
+    }
+    useEffect(()=>{
+        console.log("player"+player)
+    }, [player])
+    
     const items = ()=>{
         const spaces = []
         for(let i = 0; i<9;i++){
 
             spaces.push(
-                <Casilla key={i} callback={tooglePlayer} player={player} />
+                <Casilla key={i} id={`${Math.floor(i/3)}-${i%3}`} callback={callback} player={player} gameState={gameState}/>
             )
         
         }
         return spaces
     }
-    const [player, tooglePlayer] = usePlayer(null)
+    
     return(
+        <>
         <div
         style={{display:"grid", gridTemplateColumns:"1fr 1fr 1fr"}}
         className="tablero">
             {items()}
         </div>
+        <p>{gameState}</p>
+        </>
     )
 }
 
